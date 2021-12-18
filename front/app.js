ec00cc6c03ed92db2701fbfe0246a5809804fc73
xrybrsx <55786389+xrybrsx@users.noreply.github.com>
2021-12-18 17:35:39 +0200

    express-sesion module

--- front/app.js
+++ front/app.js
@@ -1,12 +1,24 @@
 'use strict';
 const express = require('express');
+var session = require('express-session');
 const app = express();
 const server = require('http').Server(app);
 //Setup static page handling
 app.set('view engine', 'ejs');
+//set up session 
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
 app.use('/static', express.static('public'));
 app.get('/', (req, res) => {
     res.render('template');
+});
+app.post('/auth', function(request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    console.log("Username is: " + username + " and password is: " + password);
 });
 
 function startServer() {
